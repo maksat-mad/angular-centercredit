0b2d2634e08beaf96c5fe1d9c6d9cc8b705713b0
Maksat <maksat.madeniyetov@gmail.com>
2023-11-25 16:15:09 +0600

    implement design of home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,13 +6,4 @@ import { Component, inject } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  show = false;
-
-  handleFaceClick() {
-    this.show = !this.show;
-  }
-
-  handleBack() {
-    this.show = !this.show;
-  }
 }
